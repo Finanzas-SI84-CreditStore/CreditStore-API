364e2bb7fa145f0226052de5ae1352b02d48be30
Josef Romero Florida <josef.romeroflorida@gmail.com>
2024-05-17 12:04:30 -0500

    feat: First Commit

---
+++ src/main/java/com/creditstore/CreditStore/CreditStoreApplication.java
@@ -0,0 +1,13 @@
+package com.creditstore.CreditStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreditStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreditStoreApplication.class, args);
+	}
+
+}
