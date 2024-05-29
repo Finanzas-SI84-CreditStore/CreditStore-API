7c8157d03420068f1ba68ec5c05c9e220e6e4e0f
Jennifer Espinoza <jenniespinoza2002@hotmail.com>
2024-05-29 12:01:54 -0500

    feat: add client entity

---
+++ src/main/java/com/creditstore/CreditStore/clients/model/ClientDto.java
@@ -0,0 +1,38 @@
+package com.creditstore.CreditStore.clients.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import java.time.LocalDate;
+import java.util.UUID;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@JsonInclude(Include.NON_NULL)
+public class ClientDto {
+
+  private final UUID id;
+
+  private final String name;
+
+  private final String lastName;
+
+  private final String dni;
+
+  private final LocalDate birthDate;
+
+  private final String address;
+
+  private final int paymentDay;
+
+  private final double creditLine;
+
+  private double debt;
+
+  private double availableBalance;
+
+
+}
---
+++ src/main/java/com/creditstore/CreditStore/clients/model/ClientQuery.java
@@ -0,0 +1,35 @@
+package com.creditstore.CreditStore.clients.model;
+
+import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ClientQuery {
+  private String name;
+
+  private String lastName;
+
+  private String dni;
+
+  private LocalDate birthDate;
+
+  private String address;
+
+  private int paymentDay;
+
+  private double creditLine;
+
+  private double debt;
+
+  private double availableBalance;
+}
