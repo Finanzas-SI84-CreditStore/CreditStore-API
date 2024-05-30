bf0e608c8fb58c3fbcf772eb73fd064ebe7600a5
Jennifer Espinoza <jenniespinoza2002@hotmail.com>
2024-05-30 00:07:35 -0500

    feat: add cross origin

--- src/main/java/com/creditstore/CreditStore/security/rest/UserRest.java
+++ src/main/java/com/creditstore/CreditStore/security/rest/UserRest.java
@@ -14,14 +14,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("users")
+@CrossOrigin(origins = "*")
 public class UserRest {
 
   private static final Logger logger = LoggerFactory.getLogger(UserRest.class);
